c1b64608fb31feb5e6832fa7a9e6970743987879
Guido Santillan <guidosantillan01@gmail.com>
2019-06-26 17:15:06 -0500

    Relational data: arrays

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
 
   type Post {
@@ -118,6 +119,13 @@ const resolvers = {
     author(parent, args, ctx, info) {
       return users.find(user => {
         return user.id === parent.author;
+      });
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => {
+        return post.author === parent.id;
       });
     }
   }
