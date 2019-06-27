e74128cfa8375e0a6734615e82e22931e82cfd30
Guido Santillan <guidosantillan01@gmail.com>
2019-06-26 21:23:11 -0500

    Updating data with mutations: updatePost and updateComment

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -97,6 +97,29 @@ const Mutation = {
 
     return deletedPosts[0]; // Return the first and only item from the array
   },
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+
+    const post = db.posts.find(post => post.id === id);
+
+    if (!post) {
+      throw new Error('Post not found');
+    }
+
+    if (typeof data.title === 'string') {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === 'string') {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+    }
+
+    return post;
+  },
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
     const postExists = db.posts.some(
@@ -128,6 +151,21 @@ const Mutation = {
     const deletedComments = db.comments.splice(commentIndex, 1);
 
     return deletedComments[0];
+  },
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+
+    const comment = db.comments.find(comment => comment.id === id);
+
+    if (!comment) {
+      throw new Error('Comment not found');
+    }
+
+    if (typeof data.text === 'string') {
+      comment.text = data.text;
+    }
+
+    return comment;
   }
 };
 
