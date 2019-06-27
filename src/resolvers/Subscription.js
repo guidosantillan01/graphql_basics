4f8535deba5937f0c572cd0c8bfd36980cb5da8e
Guido Santillan <guidosantillan01@gmail.com>
2019-06-27 10:26:36 -0500

    Setting up a Comments Subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,21 +1,7 @@
 const Subscription = {
   // Object
-  count: {
-    // Method
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0;
-
-      setInterval(() => {
-        count++;
-        pubsub.publish('count', {
-          count
-        });
-      }, 1000);
-
-      return pubsub.asyncIterator('count');
-    }
-  },
   comment: {
+    // Method
     subscribe(parent, { postId }, { db, pubsub }, info) {
       const post = db.posts.find(post => post.id === postId && post.published);
 
